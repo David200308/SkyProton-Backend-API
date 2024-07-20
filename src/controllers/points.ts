55d6779744bffa81cf7dd995c080e836c5a61cf6
David Jiang <jiangguanlin2003@outlook.com>
2024-07-20 20:22:33 +0800

    fix: the bugs of add points

--- src/controllers/points.ts
+++ src/controllers/points.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Get, HttpStatus, Param, Post, Req, Res } from '@nestjs/common';
-// import { verifyToken } from '../utils/auth';
+import { verifyToken } from '../utils/auth';
 import { Response, Request } from 'express';
-// import { UNAUTHORIZED } from '../const/user';
+import { UNAUTHORIZED } from '../const/user';
 import { PointsServices } from 'src/services/points';
 
 @Controller("points")
@@ -10,19 +10,19 @@ export class PointsController {
 
     @Get(":id")
     async getUserPoints(@Param('id') id: string, @Req() request: Request, @Res({ passthrough: true }) response: Response) {
-        // if (!request.cookies.token)
-        //     return response.status(HttpStatus.UNAUTHORIZED).json({
-        //         message: UNAUTHORIZED,
-        //     });
+        if (!request.cookies.token)
+            return response.status(HttpStatus.UNAUTHORIZED).json({
+                message: UNAUTHORIZED,
+            });
 
-        // const token = request.cookies.token;
-        // await verifyToken(token).catch((err) => {
-        //     console.log(err);
-        //     return response.status(HttpStatus.UNAUTHORIZED).json({
-        //         message: UNAUTHORIZED,
-        //         error: err
-        //     });
-        // });
+        const token = request.cookies.token;
+        await verifyToken(token).catch((err) => {
+            console.log(err);
+            return response.status(HttpStatus.UNAUTHORIZED).json({
+                message: UNAUTHORIZED,
+                error: err
+            });
+        });
 
         const data = await this.pointsServices.getPoints(id);
 
@@ -31,19 +31,19 @@ export class PointsController {
 
     @Post()
     async addPoints(@Body() body: { id: string, points: string }, @Req() request: Request, @Res({ passthrough: true }) response: Response) {
-        // if (!request.cookies.token)
-        //     return response.status(HttpStatus.UNAUTHORIZED).json({
-        //         message: UNAUTHORIZED,
-        //     });
+        if (!request.cookies.token)
+            return response.status(HttpStatus.UNAUTHORIZED).json({
+                message: UNAUTHORIZED,
+            });
 
-        // const token = request.cookies.token;
-        // await verifyToken(token).catch((err) => {
-        //     console.log(err);
-        //     return response.status(HttpStatus.UNAUTHORIZED).json({
-        //         message: UNAUTHORIZED,
-        //         error: err
-        //     });
-        // });
+        const token = request.cookies.token;
+        await verifyToken(token).catch((err) => {
+            console.log(err);
+            return response.status(HttpStatus.UNAUTHORIZED).json({
+                message: UNAUTHORIZED,
+                error: err
+            });
+        });
 
         const data = await this.pointsServices.addPoints(body.id, body.points);
 
