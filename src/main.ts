7168960b29b66d63ab16ed870cb6d55a2f3d1226
David Jiang <jiangguanlin2003@outlook.com>
2024-07-20 21:52:36 +0800

    fix: fix bugs

--- src/main.ts
+++ src/main.ts
@@ -13,14 +13,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    next();
-  });
-  
   app.enableCors({
     origin: '*',
   });
